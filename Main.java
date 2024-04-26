32410dcd15274cb03185bc3c45eb9da585041efa
Luisa Frugoli <luisafrugoli11@gmail.com>
2024-04-26 14:15:43 -0300

    2º dia de trabalho

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 package hotel_california;
 
 import hotel_california.Quarto.AlreadyOccupiedRoomException;
+import hotel_california.Quarto.CheckOutWithoutCheckInException;
+
+import java.time.LocalDate;
 
 public class Main {
 
@@ -16,16 +19,29 @@ public class Main {
 		Cliente marcos = new Cliente("Marcos Meireles da Silva Lopes", false);
 		Cliente luciana = new Cliente("Luciana Carmo das Dores", false);
 		Cliente marisete = new Cliente("Marisete Albuquerque Conceição", false);
-		Cliente nilo = new Cliente("Nilo Gonzaga Freitas", false);
 		Cliente jean = new Cliente("Jean-Jacques Yves Desmoulins", false);
 		Cliente flavia = new Cliente("Flávia Nunes Afonseca", false);
 		
 		// Reservas confirmadas
 		try {
-			q110.reservarQuarto(marcos);
-			q111.reservarQuarto(luciana);
-			q112.reservarQuarto(marisete);
+			q110.reservarQuarto(marcos, LocalDate.of(2024, 6, 15), LocalDate.of(2024, 6, 30));
+			q111.reservarQuarto(luciana, LocalDate.of(2024, 6, 16), LocalDate.of(2024, 5, 10));
+			q112.reservarQuarto(marisete, LocalDate.of(2024, 12, 1), LocalDate.of(2025, 1, 30));
 		} catch (AlreadyOccupiedRoomException e) {
+			e.printStackTrace();
+		}
+		
+		// Clientes que fizeram checkOut
+		try {
+			q111.liberarQuarto();
+		} catch(CheckOutWithoutCheckInException e) {
+			e.printStackTrace();
+		}
+		
+		// AlreadyOccupiedRoomException
+		try {
+			q112.reservarQuarto(flavia, LocalDate.of(2025, 7, 14), LocalDate.of(2025, 8, 19));
+		} catch(AlreadyOccupiedRoomException e) {
 			e.printStackTrace();
 		}
 	}
